0228186ea2349d9f4bc6b91c675edd564eb02b55
iamco <neilcore22@gmail.com>
2025-09-01 22:49:40 +0800

    add OrganizationException interface class.

---
+++ merra-accounting/organization/src/main/java/org/merra/exceptions/OrganizationExceptions.java
@@ -0,0 +1,7 @@
+package org.merra.exceptions;
+
+public interface OrganizationExceptions {
+	static final String NOT_FOUND_INVOICE = "Invoice object not found.";
+	static final String NOT_FOUND_ACCOUNT_LOOKUP = "Account lookup not found.";
+	static final String NOT_FOUND_CONTACT_OBJ = "Contact object cannot be found.";
+}
